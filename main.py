3d63a761c45b7e4f0b53667d4305b7fde6dc93b2
johnbeatty02 <johnbeatty02@gmail.com>
2022-02-15 19:49:06 -0500

    Added sorting capabilities

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 import binpacking
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 numberOfTeams = 4
 
@@ -34,17 +34,23 @@ team2 = list(zip(*b))
 team3 = list(zip(*c))
 team4 = list(zip(*d))
 
+team1Points = list(team1[1])
+team2Points = list(team2[1])
+team3Points = list(team3[1])
+team4Points = list(team4[1])
+allPoints = [team1Points, team2Points, team3Points, team4Points]
+
 # calculates each team's average points
-points1 = round(sum(team1[1])/4, 2)
-points2 = round(sum(team2[1])/4, 2)
-points3 = round(sum(team3[1])/4, 2)
-points4 = round(sum(team4[1])/4, 2)
+points1 = round(sum(team1Points)/4, 2)
+points2 = round(sum(team2Points)/4, 2)
+points3 = round(sum(team3Points)/4, 2)
+points4 = round(sum(team4Points)/4, 2)
 
 # calculates the standard deviation of each team
-stDev1 = round(np.std(team1[1]), 2)
-stDev2 = round(np.std(team2[1]), 2)
-stDev3 = round(np.std(team3[1]), 2)
-stDev4 = round(np.std(team4[1]), 2)
+stDev1 = round(np.std(team1Points), 2)
+stDev2 = round(np.std(team2Points), 2)
+stDev3 = round(np.std(team3Points), 2)
+stDev4 = round(np.std(team4Points), 2)
 
 # prints results
 print('Team 1 is', team1[0])
@@ -59,5 +65,14 @@ print('Team 4 Average is', points4, 'with a standard deviation of', stDev4)
 
 #graphs!
 labels = ['Team 1', 'Team 2', 'Team 3', 'Team 4']
-print(team1[1])
+fig, ax =plt.subplots()
+ax.set_title('Points by Team')
+data = np.array(allPoints)
+x = np.arange(data.shape[0])
+ax.set_ylabel('Points')
 
+for i in range(data.shape[1]):
+    bottom=np.sum(data[:,0:i], axis=1)
+    ax.bar(labels,data[:,i], bottom=bottom, label="Team {}".format(i+1))
+
+plt.show()
